f091f58200497ce95646a9750da5d6d6f0843498
Daniel Spaude <daniel@spaude.de>
2012-12-29 15:59:25 +0100

    set default language to en

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -56,7 +56,7 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     #return unless params[:locale]
-    I18n.locale = 'pt'#params[:locale]
+    I18n.locale = 'en'#params[:locale]
     #return unless current_user
     #current_user.update_attribute :locale, params[:locale] if params[:locale] != current_user.locale
   end
@@ -67,7 +67,7 @@ class ApplicationController < ActionController::Base
     new_locale = current_user.locale if current_user
     new_locale = session[:locale] if session[:locale]
     unless new_locale
-      new_locale = 'pt' #request.compatible_language_from(I18n.available_locales.map(&:to_s))
+      new_locale = 'en' #request.compatible_language_from(I18n.available_locales.map(&:to_s))
       new_locale = I18n.default_locale.to_s unless new_locale
       #flash[:locale] = t('notify_locale', :locale => new_locale)
     end
